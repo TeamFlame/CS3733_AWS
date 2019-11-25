0e22f426e44f9bb199811b72023caaea0431fc04
Sam Talpey <sjtalpey@gmail.com>
2019-11-25 16:00:31 -0500

    Added localhost app file for testing

---
+++ webApp/test.js
@@ -0,0 +1,16 @@
+// File intended only for testing on localhost
+// Be sure to run 'npm install express' before attempting to launch
+
+const express = require('express'), app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/participant.html');
+});
+
+app.get('/admin', function(req, res) {
+  res.sendFile(__dirname + '/admin.html');
+});
+
+const listener = app.listen(process.env.PORT || 3000, function() {
+  console.log('App listening on port ' + listener.address().port);
+});
