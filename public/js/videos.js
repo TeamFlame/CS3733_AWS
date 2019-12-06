09c191b69b6465500f76869680bb799e9120515f
Sam Talpey <sjtalpey@gmail.com>
2019-12-06 18:30:46 -0500

    Working on local search functionality

--- public/js/videos.js
+++ public/js/videos.js
@@ -73,28 +73,96 @@ function displayVideos(videoList, isAdmin, section) {
 function searchSegment(char, text) {
   console.log('Searching text:', text);
   console.log('Searching character:', char);
+
+  // Determine if search is valid
+  // TODO check character exists?
+  if(char === '' && text === '') {return;}
+
   var xhr = new XMLHttpRequest();
   // TODO Define URL used here
-  //xhr.open('POST', getPlaylists_url, true);
-  xhr.open('POST', 'https://sl9n39xipj.execute-api.us-east-1.amazonaws.com/alpha/searchVideo', true);
-  xhr.send({
-    character: char,
-    dialogue: text
-  });
+  //xhr.open('GET', getVideos_url, true);
+  xhr.open('GET', 'https://sl9n39xipj.execute-api.us-east-1.amazonaws.com/alpha/videos', true);
+  xhr.send();
 
   xhr.onloadend = function() {    
     if(xhr.readyState == XMLHttpRequest.DONE) {
       console.log('XHR:' + xhr.response);
-      // Do something
+      displaySearch(xhr.response, char, text, searchType);
     }
   };
 
 };
 
+/**
+ * Searches for and displaysz results of a given search query
+ * 
+ * TODO
+ */
+function displaySearch(videoList, char, text, searchType) {
+  console.log('Displaying search results');
+  var segmentSection = document.getElementById(section);
+  segmentSection.innerHTML = '';
+  var js = JSON.parse(videoList);
+  console.log(js);
+  var videoList = js.list;
+
+  // Search using character and text fields
+  var charSearch = searchByChar(videoList, char);
+  var textSearch = searchByText(videoList, text);
+
+  // Get overlap if both have videos (search via both vs by one)
+
+  // Conduct search and update here
+  for(let x = 0; x < videoList.length; x++) {
+
+  }
+
+  // For each video 
+  for(let i = 0; i < searchList.length; i++) {
+    if(i % 5 === 0) {
+      segmentSection.innerHTML += '<br><br>'
+    }
+    let video = searchList[i];
+    console.log(video);
+    //  create and append a new video element
+    var videoElement = document.createElement('video');
+
+    // Set attributes
+    videoElement.setAttribute('src', video.bucketURI);
+    videoElement.setAttribute('width', '400');
+    videoElement.setAttribute('height', '300');
+    videoElement.setAttribute('controls', 'controls');
+
+    // Append to doc
+    segmentSection.appendChild(videoElement);
+
+    
+    // Add marking button for admins
+    if(isAdmin) {
+      let markButton = document.createElement('input');
+      markButton.setAttribute('type', 'button');
+      markButton.setAttribute('value', 'Mark for Remote Access');
+      markButton.setAttribute('disabled', 'disabled');
+      segmentSection.appendChild(markButton);
+      segmentSection.innerHTML += '<br>';
+    }
+  };
+}
+
+/**
+ * Uploads a given videosegment tot he application library
+ * 
+ * TODO determine what form the input will come in (string?)
+ */
 function uploadSegment() {
 
 };
 
-function deleteSegment() {
+/**
+ * Delete a given vcideo segment from the library
+ * 
+ * TODO 
+ */
+function deleteSegment(video) {
 
 };
