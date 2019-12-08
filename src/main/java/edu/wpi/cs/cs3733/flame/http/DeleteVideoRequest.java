4ce6b4fede1e62bd3cfe759ca46894d24323a59f
ryanc <TheRyanCirella>
2019-12-07 20:39:38 -0500

    Creating tests for delete video segment//Fix playlist handler test

--- src/main/java/edu/wpi/cs/cs3733/flame/http/DeleteVideoRequest.java
+++ src/main/java/edu/wpi/cs/cs3733/flame/http/DeleteVideoRequest.java
@@ -3,7 +3,7 @@ package edu.wpi.cs.cs3733.flame.http;
 public class DeleteVideoRequest {
 	public String bucketURI;
 	public String getBucketURI() { return bucketURI; }
-	public void setName(String b) { bucketURI = b; }
+	public void setBucketURI(String b) { bucketURI = b; }
 	
 	public DeleteVideoRequest() { // We actually need the null contstructor so the Jackson mapper understands the request
 	}
